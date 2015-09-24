e81cc422396c340e8f50f323c6abce3e5fdd8563
WangYg <wangyonggang@ctyun.cn>
2015-09-24 17:52:31 +0800

    使用新的压缩发布文件 重写了基本组件demo

--- base-demo/main.js
+++ base-demo/main.js
@@ -1,11 +1,17 @@
-var React=require("react");
-var components=require("components");
+/**
+ * 基本组件的展示
+ * 
+ */
+var React = components.React;
 var Button = components.Button;
 
 var ThisPage = React.createClass({
+	onClick: function(){
+		alert("点击了");
+	},
     render: function(){
         return(<div>
-                <Button btnName="演示button"/>
+                <Button btnName="演示button" doAction={this.onClick}/>
             </div>);
     }
 });
