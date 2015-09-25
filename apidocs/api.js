05eb846534fb9665a952f945418b66a7d14dfe0a
WangYg <wangyonggang@ctyun.cn>
2015-09-25 13:44:19 +0800

    更新API文档

---
+++ apidocs/api.js
@@ -0,0 +1,192 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AutoSelect",
+        "Autocomplete",
+        "BSSForm",
+        "BSSPanel",
+        "BasicArea",
+        "BasicColumn",
+        "BasicLine",
+        "BasicPie",
+        "Button",
+        "CascadeSelect",
+        "Checkbox",
+        "CheckboxGroup",
+        "DatePicker",
+        "ECharts",
+        "ExpressionSelect",
+        "Grid",
+        "Input",
+        "IrregularLine",
+        "MessageBox",
+        "Modal",
+        "PageButton",
+        "PageSizeSelect",
+        "QueryPanel",
+        "Select",
+        "StackedArea",
+        "StackedColumn",
+        "StackedLine",
+        "TablePanel",
+        "Tabs",
+        "TencentMap",
+        "Textarea",
+        "ToolBarPanel",
+        "TreeCommonMixin",
+        "Treeview",
+        "TreeviewCell",
+        "TreeviewNode",
+        "TreeviewTextBox",
+        "WordCloud",
+        "message",
+        "window"
+    ],
+    "modules": [
+        "BSSPanel",
+        "BasicArea",
+        "BasicColumn",
+        "BasicLine",
+        "BasicPie",
+        "ECharts",
+        "Header",
+        "IrregularLine",
+        "MessageBox",
+        "Modal",
+        "PageButton",
+        "PageSizeSelect",
+        "QueryPanel",
+        "StackedArea",
+        "StackedColumn",
+        "StackedLine",
+        "TablePanel",
+        "Tabs",
+        "TencentMap",
+        "ToolBarPanel",
+        "Treeview",
+        "WordCloud",
+        "html",
+        "message"
+    ],
+    "allModules": [
+        {
+            "displayName": "BasicArea",
+            "name": "BasicArea",
+            "description": "BasicArea Chart ,using EChart"
+        },
+        {
+            "displayName": "BasicColumn",
+            "name": "BasicColumn",
+            "description": "BasicColumn Chart ,using EChart"
+        },
+        {
+            "displayName": "BasicLine",
+            "name": "BasicLine",
+            "description": "BasicLine Chart ,using EChart"
+        },
+        {
+            "displayName": "BasicPie",
+            "name": "BasicPie",
+            "description": "BasicPie Chart ,using EChart"
+        },
+        {
+            "displayName": "BSSPanel",
+            "name": "BSSPanel",
+            "description": "Created by chenth on 15-7-24.\n基本的面板组件"
+        },
+        {
+            "displayName": "ECharts",
+            "name": "ECharts",
+            "description": "ECharts"
+        },
+        {
+            "displayName": "Header",
+            "name": "Header"
+        },
+        {
+            "displayName": "html",
+            "name": "html"
+        },
+        {
+            "displayName": "IrregularLine",
+            "name": "IrregularLine",
+            "description": "IrregularLine Chart ,using EChart"
+        },
+        {
+            "displayName": "message",
+            "name": "message",
+            "description": "MessageBox MessageBox方法集合"
+        },
+        {
+            "displayName": "MessageBox",
+            "name": "MessageBox",
+            "description": "消息提示框组件"
+        },
+        {
+            "displayName": "Modal",
+            "name": "Modal",
+            "description": "Modal组件"
+        },
+        {
+            "displayName": "PageButton",
+            "name": "PageButton",
+            "description": "Created by chenth on 15-7-10.\n表格分页组件"
+        },
+        {
+            "displayName": "PageSizeSelect",
+            "name": "PageSizeSelect",
+            "description": "Created by chenth on 15-7-10.\n 页面大小选择组件"
+        },
+        {
+            "displayName": "QueryPanel",
+            "name": "QueryPanel",
+            "description": "Created by chenth on 15-7-24.\n查询面板,包装了form表单"
+        },
+        {
+            "displayName": "StackedArea",
+            "name": "StackedArea",
+            "description": "StackedArea Chart ,using EChart"
+        },
+        {
+            "displayName": "StackedColumn",
+            "name": "StackedColumn",
+            "description": "StackedColumn Chart ,using EChart"
+        },
+        {
+            "displayName": "StackedLine",
+            "name": "StackedLine",
+            "description": "StackedLine Chart ,using EChart"
+        },
+        {
+            "displayName": "TablePanel",
+            "name": "TablePanel",
+            "description": "Created by chenth on 15-7-24.\n封装表格面板组件"
+        },
+        {
+            "displayName": "Tabs",
+            "name": "Tabs"
+        },
+        {
+            "displayName": "TencentMap",
+            "name": "TencentMap",
+            "description": "地图模块(使用腾讯地图)"
+        },
+        {
+            "displayName": "ToolBarPanel",
+            "name": "ToolBarPanel",
+            "description": "Created by chenth on 15-7-24.\n工具栏面板"
+        },
+        {
+            "displayName": "Treeview",
+            "name": "Treeview",
+            "description": "This is a Treeview component."
+        },
+        {
+            "displayName": "WordCloud",
+            "name": "WordCloud",
+            "description": "WordCloud Chart ,using EChart"
+        }
+    ],
+    "elements": []
+} };
+});
