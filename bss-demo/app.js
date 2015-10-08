4b1994c8c6217f823eadf2ec4d9bfec774531164
WangYg <wangyonggang@ctyun.cn>
2015-10-08 10:14:55 +0800

    这是旧路由的最后一个版本

--- bss-demo/app.js
+++ bss-demo/app.js
@@ -1,6 +1,7 @@
 var React=components.React
 var Button = components.Button;
 var Demo = components.Demo;
+var Login = components.Login;
 
 
 var Test=React.createClass({
@@ -15,10 +16,23 @@ var Test2=React.createClass({
         return (<div>test2</div>);
     }
 });
+var Login1 = React.createClass({
+    render:function(){
+        console.log(" in /Login1 ");
+        return(<Login />);
+    }
+});
+
+
+
 RouteConfig["#test1"]=function(){
     RouteConfig.renderHandle(<Test />);
 };
 
 RouteConfig["#test2"]=function(){
     RouteConfig.renderHandle(<Test2 />);
+};
+
+RouteConfig["/Login"]=function(){
+    RouteConfig.renderHandle(<Login1 />);
 };
